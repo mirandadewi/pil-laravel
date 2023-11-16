79050df9767e4c82165ef1b5375218d83af6f7df
mirandadewi <mirandadewi190@gmail.com>
2023-11-16 13:53:46 +0700

    Membuat Tabel

--- resources/views/Mahasiswa.blade.php
+++ resources/views/Mahasiswa.blade.php
@@ -28,13 +28,49 @@
       </ul>
       <form class="d-flex" role="search">
         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
+        <button class="btn btn-outline-danger" type="submit">Search</button>
       </form>
     </div>
   </div>
 </nav>
     <div class="container">
     <h1>Halaman Mahasiswa</h1>
+
+
+
+    <table class="table table-danger table-sm table-hover table-striped table-border text-center">
+      <thead>
+    <tr>
+      <th>NPM</th>
+      <th>Nama Mahasiswa</th>
+      <th>Jenis Kelamin</th>
+      <th colspan="2"> Tempat Tanggal Lahir</th>
+    </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>127</td>
+          <td>Bubu</td>
+          <td>Laki Laki</td>
+          <td>06 Juni 2000</td>
+          <td>Seoul</td>
+        </tr>
+        <tr>
+          <td>128</td>
+          <td>Bubi</td>
+          <td>Laki Laki</td>
+          <td>07 Juni 2000</td>
+          <td>Busan</td>
+        </tr>
+        <tr>
+          <td>129</td>
+          <td>Bubo</td>
+          <td>Laki Laki</td>
+          <td>08 Juni 2000</td>
+          <td>Jeju</td>
+        </tr>
+      </tbody>
+    </table>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
